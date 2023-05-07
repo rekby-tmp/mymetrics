72d4caefdb3921ebed22c0acbf7775023042a807
Timofey Koolin <timofey.koolin@gmail.com>
2023-05-07 11:35:17 +0300

    add flag support

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,8 +1,36 @@
 package main
 
-import "time"
+import (
+	"flag"
+	"log"
+	"strings"
+	"time"
+
+	"github.com/rekby-tmp/mymetrics/internal/agent"
+)
+
+type Config struct {
+	Endpoint       string
+	ReportInterval time.Duration
+	PollInterval   time.Duration
+}
 
 func main() {
-	agent := NewAgent("http://localhost:8080", time.Second*2, time.Second*10)
+	var cfg Config
+	flag.StringVar(&cfg.Endpoint, "a", "localhost:8080", "endpoint")
+	pollIntervalSeconds := flag.Int("p", 2, "Poll interval (seconds)")
+	reportIntervalSeconds := flag.Int("r", 10, "Report interval")
+	flag.Parse()
+
+	cfg.PollInterval = time.Duration(*pollIntervalSeconds) * time.Second
+	cfg.ReportInterval = time.Duration(*reportIntervalSeconds) * time.Second
+
+	if !strings.HasPrefix(cfg.Endpoint, "http://") {
+		cfg.Endpoint = "http://" + cfg.Endpoint
+	}
+
+	log.Printf("Start agent with config: %#v\n", cfg)
+
+	agent := agent.NewAgent(cfg.Endpoint, cfg.PollInterval, cfg.ReportInterval)
 	agent.Start()
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"log"
 	"net/http"
 
 	"github.com/rekby-tmp/mymetrics/internal/server"
 )
 
+type Config struct {
+	Endpoint string
+}
+
 func main() {
-	s := server.NewServer("localhost:8080", server.NewMemStorage())
+	var cfg Config
+	flag.StringVar(&cfg.Endpoint, "a", "localhost:8080", "Endpoint")
+	flag.Parse()
+
+	log.Printf("Start server with config: %#v\n", cfg)
+	s := server.NewServer(cfg.Endpoint, server.NewMemStorage())
 	err := s.Start()
 	if !errors.Is(err, http.ErrServerClosed) {
 		log.Fatal(err)
